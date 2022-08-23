3f1798d00f9b175062fee27c20bb9d8bd7afd843
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-08-22 22:55:57 -0500

    can't get product post to work but created product delete.  Going to make a feature for deletes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -74,8 +74,7 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create({
-  })
+  Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -140,22 +139,23 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  // delete one product by its `id` value 
   Product.destroy({
     where: {
-      id: req.params.id
+        id: req.params.id
     }
   })
     .then(dbProductData => {
-      if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
-        return;
-      }
-      res.json(dbProductData);
-    })
+        if (!dbProductData) {
+            res.status(404).json({ message: 'No product found with this id'});
+            return;
+        }
+        res.json(dbProductData);
+  })
     .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+        console.log(err);
+        res.status(500).json(err);
+  });
 });
+
 module.exports = router;
